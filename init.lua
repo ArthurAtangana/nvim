3f383e23ebcacbe8e00090f45ee5f037ad9df59e
ArthurAtangana <arthur.atangana@protonmail.com>
2025-11-16 20:13:44 -0800

    Add pairs plugin, indent line plugins, gitsigns keymaps

--- init.lua
+++ init.lua
@@ -924,6 +924,7 @@ require('lazy').setup({
         require('mini.files').open()
       end, { desc = 'Open [E]xplorer' })
 
+      require('mini.pairs').setup()
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
@@ -964,11 +965,11 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
